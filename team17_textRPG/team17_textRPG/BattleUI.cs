2c8f78191916a2702c3b568f8409fe9cf7dd0f75
ParkMinsoo <harimaho88@gmail.com>
2025-04-21 17:38:25 +0900

    BattleUI

--- team17_textRPG/team17_textRPG/BattleUI.cs
+++ team17_textRPG/team17_textRPG/BattleUI.cs
@@ -10,7 +10,7 @@ namespace team17_textRPG
 {
     internal class BattleUI
     {
-        private Random rand = new Random();
+        public Random rand = new Random();
 
         public void CharacterAttack(Character character, Monster monster)
         {
@@ -22,40 +22,34 @@ namespace team17_textRPG
 
         public void MonsterAttack(Monster monster, Character character)
         {
-            int errorRangeMA = (int)Math.Ceiling(moster.Atk * 0.1);
-            int damage = rand.Next(monster.Atk - errorRangeMA, moster.Atk + errorRangeMA + 1);
-            Console.WriteLine($"{monster.DisplayName()} 의 공격!");
+            int errorRangeMA = (int)Math.Ceiling(monster.Atk * 0.1);
+            int damage = rand.Next(monster.Atk - errorRangeMA, monster.Atk + errorRangeMA + 1);
+            Console.WriteLine($"{monster.Level} {monster.Name} 의 공격!");
             ApplyDamage(character, damage);
         }
-        private void ApplyDamage(Character character, int damage)
+        public void ApplyDamage(Character character, int damage)
         {
-            int originalHp = player.Hp;
-            player.Hp -= damage;
-            if (player.Hp < 0) player.Hp = 0;
-            Console.WriteLine($"\nLv.{player.Level} {player.Name} 을(를) 맞췄습니다.  [데미지 : {damage}]");
-            Console.WriteLine($"Lv.{player.Level} {player.Name}\nHP {originalHp} -> {player.Hp}");
+            int originalHp = character.Hp;
+            character.Hp -= damage;
+            if (character.Hp < 0) character.Hp = 0;
+            Console.WriteLine($"\nLv.{character.Lv} {character.Name} 을(를) 맞췄습니다.  [데미지 : {damage}]");
+            Console.WriteLine($"Lv.{character.Lv} {character.Name}\nHP {originalHp} -> {character.Hp}");
         }
 
-        private void ApplyDamage(Monster monster, int damage)
+        public void ApplyDamage(Monster monster, int damage)
         {
-            if (!monster.IsAlive)
-            {
-                Console.WriteLine("잘못된 입력입니다 (이미 죽은 몬스터입니다).");
-                return;
-            }
-
             int originalHp = monster.Hp;
             monster.Hp -= damage;
             if (monster.Hp <= 0)
             {
                 monster.Hp = 0;
-                Console.WriteLine($"\n{monster.DisplayName()} 을(를) 맞췄습니다. [데미지 : {damage}]");
-                Console.WriteLine($"{monster.DisplayName()}\nHP {originalHp} -> Dead");
+                Console.WriteLine($"\n{monster.Level} {monster.Name} 을(를) 맞췄습니다. [데미지 : {damage}]");
+                Console.WriteLine($"{monster.Level} {monster.Name}\nHP {originalHp} -> Dead");
             }
             else
             {
-                Console.WriteLine($"\n{monster.DisplayName()} 을(를) 맞췄습니다. [데미지 : {damage}]");
-                Console.WriteLine($"{monster.DisplayName()}\nHP {originalHp} -> {monster.Hp}");
+                Console.WriteLine($"\n{monster.Level} {monster.Name} 을(를) 맞췄습니다. [데미지 : {damage}]");
+                Console.WriteLine($"{monster.Level} {monster.Name}\nHP {originalHp} -> {monster.Hp}");
             }
         }
 
