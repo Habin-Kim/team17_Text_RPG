8bd49d5c4f56f1e787ac9dce05af652420e0c9d5
Yusang-1 <tearless25hg@gmail.com>
2025-04-21 17:24:54 +0900

    Update Program.cs

--- team17_textRPG/team17_textRPG/Program.cs
+++ team17_textRPG/team17_textRPG/Program.cs
@@ -52,10 +52,11 @@
                 bool isNumber = int.TryParse(input, out result);
                 if (isNumber)
                 {
-                    if(result >= min && result <= max)
+                    if (result >= min && result <= max)
                         return result;
                 }
                 Console.WriteLine("잘못된 입력입니다!");
             }
         }
+    }
 }
