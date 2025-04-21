da18a8289c20b3c73278a2566cc88c04557e5e5f
Yusang-1 <tearless25hg@gmail.com>
2025-04-21 17:19:40 +0900

    add : 시작화면
    - 시작화면

--- team17_textRPG/team17_textRPG/Program.cs
+++ team17_textRPG/team17_textRPG/Program.cs
@@ -2,13 +2,36 @@
 {
     internal class Program
     {
+        private static Character character;
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            character = new Character();
+            StartScene();
         }
 
+        static void StartScene()
+        {
+            int result;
+            Console.Clear();
+            Console.WriteLine("\n스파르타 던전에 오신 여러분 환영합니다.");
+            Console.WriteLine("이제 전투를 시작할 수 있습니다.");
+            Console.WriteLine("\n1. 상태 보기\n2. 전투 시작");
+            Console.WriteLine("\n원하시는 행동을 입력해 주세요.");
+            Console.Write(">>");
+            result = CheckInput(1, 2);
+            switch (result)
+            {
+                case 1:
+                    character.ShowStats();
+                    result = CheckInput(0, 0);
+                    StartScene();
+                    break;
+                case 2:
+                    //전투시작
+                    break;
+            }
+        }
 
-        
         static int CheckInput(int min, int max)
         {
             int result;
