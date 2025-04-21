08500acfb0caa1b44c822b0e29fc3771bea1d241
Yusang-1 <tearless25hg@gmail.com>
2025-04-21 13:39:17 +0900

    add : 메인 스크립트 추가
    - 메인 스크립트 추가

---
+++ team17_textRPG/team17_textRPG/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace team17_textRPG
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
