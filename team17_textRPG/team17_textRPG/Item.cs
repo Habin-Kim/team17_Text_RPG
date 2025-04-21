d791674e12c54514f2772c8b79bdeb8b0e8a081d
song-ssi <song33@outlook.kr>
2025-04-21 18:03:31 +0900

    포션사용 코드 작성

--- team17_textRPG/team17_textRPG/Item.cs
+++ team17_textRPG/team17_textRPG/Item.cs
@@ -18,10 +18,14 @@ namespace team17_textRPG
     class Item
     {   
         static int hpPotion = 3;
-    
+        public int maxHp = 100;
+        Program program = new Program();
+        Character player = new Character();
+        int currentHP = player.Hp;
 
         public void DisplayHealUI()
         {
+            Console.Clear();
             Console.WriteLine("회복");
             Console.WriteLine($"포션을 사용하면 체력을 30 회복할 수 있습니다. (남은 포션 : {hpPotion})");
             Console.WriteLine();
@@ -30,11 +34,55 @@ namespace team17_textRPG
             Console.WriteLine();
             Console.WriteLine("원하시는 행동을 입력해주세요");
             Console.Write(">>");
+
+            int result = program.CheckInput(0,1);
+
+            switch(result)
+            {
+                case 0:
+                {
+                    break;;
+                }
+                case 1:
+                    UseHpPotion();
+                    break;
+            }
+        }
+
+        public void UseHpPotion()
+        {
+            if (hpPotion > 0)
+            {
+                if(Hp < maxHp)
+                {
+                    Console.WriteLine("체력이 회복되었습니다.");
+                    Hp = Math.Min(Hp + 30, maxHp); // 둘 중 작은 값 출력;
+                }
+                else
+                {
+                    Console.WriteLine("체력을 회복할 수 없습니다.");
+                }
+            }
+            else
+            {
+                Console.WriteLine("포션이 부족합니다.");
+                Console.WriteLine();
+                Console.WriteLine("0. 나가기");
+
+                int result = program.CheckInput(0,0);
+                switch(result)
+                {
+                    case 0:
+                    {
+                        break;
+                    }
+                }
+            }
         }
 
         public void DisplayInventory()
         {
-            
+
         }
     }
 }
