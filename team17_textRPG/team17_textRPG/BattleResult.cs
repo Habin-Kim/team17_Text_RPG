c45fc26a6ad9ab32fc1a1044b2367cd7a56570fb
SnowAqua55 <63825945+SnowAqua55@users.noreply.github.com>
2025-04-22 19:46:25 +0900

    Update BattleResult.cs

--- team17_textRPG/team17_textRPG/BattleResult.cs
+++ team17_textRPG/team17_textRPG/BattleResult.cs
@@ -4,36 +4,38 @@
     // 전투 결과
     public class BattleResult
     {
+        
+
         // 이기면 빅토리 뜨고 체력 띄우기
-        static void Victory()
+        public void Victory()
         {
             Console.Clear();
-
+            
             Console.WriteLine("Battle!! - Result");
             Console.ForegroundColor = ConsoleColor.Green;
             Console.WriteLine("\nVictory");
             Console.ResetColor();
 
-            Console.WriteLine($"\n던전에서 몬스터 {KilledMonster}마리를 잡았습니다.");
+            Console.WriteLine($"\n던전에서 몬스터 {Monsters.monster}마리를 잡았습니다.");
 
-            Console.WriteLine("Lv. {0:D2} {1}"Lv, Name);
+            Console.WriteLine($"Lv. {Program.character.Lv:D2} {Program.character.Name}");
             Console.WriteLine($"HP {MaxHp} -> {ResultHp}");
 
             Console.WriteLine("0. 다음");
             Console.Write(">> ");
 
-            int result = CheckInput(0, 0);
+            int result = Program.CheckInput(0, 0);
 
             switch (result)
             {
                 case 0:
-                    DisplayMainUI();
+                    Program.StartScene();
                     break;
             }
         }
         
         // 지면 루즈 뜨고 체력 띄우기
-        static void Lose()
+        public void Lose()
         {
             Console.Clear();
 
@@ -42,18 +44,18 @@
             Console.WriteLine("\nYou Lose...");
             Console.ResetColor();
 
-            Console.WriteLine("Lv. {0:D2} {1}"Lv, Name);
+            Console.WriteLine($"Lv. {Program.character.Lv:D2} {Program.character.Name}");
             Console.WriteLine($"HP {MaxHp} -> {ResultHp}");
 
             Console.WriteLine("0. 다음");
             Console.Write(">> ");
 
-            int result = CheckInput(0, 0);
+            int result = Program.CheckInput(0, 0);
 
             switch (result)
             {
                 case 0:
-                    DisplayMainUI();
+                    Program.StartScene();
                     break;
             }
         }
