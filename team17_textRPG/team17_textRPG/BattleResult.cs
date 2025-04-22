fc7795b30e09f78c4897591c017b79801466a5e6
SnowAqua55 <63825945+SnowAqua55@users.noreply.github.com>
2025-04-22 09:25:05 +0900

    Update BattleResult.cs

--- team17_textRPG/team17_textRPG/BattleResult.cs
+++ team17_textRPG/team17_textRPG/BattleResult.cs
@@ -1,31 +1,16 @@
-﻿using System.Net.Security;
-using System.Reflection.Emit;
-using System.Xml.Linq;
-
-namespace TeamProject
+﻿namespace team17_textRPG
 {
     
     // 전투 결과
     public class BattleResult
     {
-        static int CheckInput(int min, int max)
-        {
-            int result;
-            while (true)
-            {
-                string input = Console.ReadLine();
-                bool isNumber = int.TryParse(input, out result);
-                if (isNumber)
-                {
-                    if (result >= min && result <= max)
-                        return result;
-                }
-                Console.WriteLine("잘못된 입력입니다.");
-            }
-        }
+        public int ResultHp;
+        private static Character Character;
 
+        public int BattleHp = Character.Hp;
+        
         // 이기면 빅토리 뜨고 체력 띄우기
-        static void Victory()
+        void Victory()
         {
             Console.Clear();
 
@@ -36,8 +21,8 @@ namespace TeamProject
 
             Console.WriteLine($"\n던전에서 몬스터 {KilledMonster}마리를 잡았습니다.");
 
-            Console.WriteLine("Lv. {0:D2} {1}", Lv, Name);
-            Console.WriteLine($"HP {RecentHp} -> {ResultHp}");
+            Console.WriteLine("Lv. {0:D2} {1}", Character.Lv, Character.Name);
+            Console.WriteLine($"HP {BattleHp} -> {ResultHp}");
 
             Console.WriteLine("0. 다음");
             Console.Write(">> ");
@@ -50,7 +35,10 @@ namespace TeamProject
                     DisplayMainUI();
                     break;
             }
+            
         }
+
+        
         
         // 지면 루즈 뜨고 체력 띄우기
         static void Lose()
@@ -62,13 +50,13 @@ namespace TeamProject
             Console.WriteLine("\nYou Lose...");
             Console.ResetColor();
 
-            Console.WriteLine("Lv. {0:D2} {1}", Lv, Name);
-            Console.WriteLine($"HP {RecentHp} -> {ResultHp}");
+            Console.WriteLine("Lv. {0:D2} {1}", Character.Lv, Character.Name);
+            Console.WriteLine($"HP {BattleHp} -> {ResultHp}");
 
             Console.WriteLine("0. 다음");
             Console.Write(">> ");
 
-            int result = CheckInput(0, 0);
+            int result = Program.CheckInput(0, 0);
 
             switch (result)
             {
