2d9256860085540eab24fa8f5911abe4a5a237e7
Yusang-1 <tearless25hg@gmail.com>
2025-04-22 14:30:30 +0900

    Update : 상점과 관련된 코드 추가
    - 상점과 관련된 코드 추가

--- team17_textRPG/team17_textRPG/Character.cs
+++ team17_textRPG/team17_textRPG/Character.cs
@@ -36,7 +36,8 @@
             currentExp = 0;
         }
 
-        List<Item> itemList = new List<Item>();
+        //List<Item> itemList = new List<Item>();
+        List<Gears> InventoryGears = new List<Gears>();
 
         public void ShowStats()
         {
@@ -56,17 +57,30 @@
 
         public void ShowInv()
         {
-            int itemCount = itemList.Count;
+            //int itemCount = itemList.Count;
+            int InvGearCount = InventoryGears.Count;
+            string eff;
             Console.Clear();
             Console.WriteLine("\n인벤토리");
             Console.WriteLine("인벤토리의 정보가 표시됩니다.\n");
-            for (int i = 0; i < itemCount; i++)
+            //for (int i = 0; i < itemCount; i++)
+            //{
+            //    Console.WriteLine($"{itemList[i].Name}   체력{itemList[i].Effect}회복");
+            //}
+            for (int i = 0; i < InvGearCount; i++)
             {
-                Console.WriteLine($"{itemList[i].Name}   체력{itemList[i].Effect}회복");
+                eff = InventoryGears[i].Type == 1 ? "공격력" : "방어력";
+                Console.WriteLine($"- {InventoryGears[i].Name}  | {eff}+{InventoryGears[i].Effect}  | {InventoryGears[i].Desc}");
             }
-            Console.WriteLine("\n1.장비  2.물약  0.나가기");
+            Console.WriteLine("\n1.장착관리  0.나가기");
             Console.WriteLine("\n원하시는 행동을 입력해 주세요.");
             Console.Write(">>");
+        }
+
+        public void BuyItem(Gears gear)
+        {
+            InventoryGears.Add(gear);
+            Gold -= gear.Price;
         }
         public void GetExp(int exp)
         {
