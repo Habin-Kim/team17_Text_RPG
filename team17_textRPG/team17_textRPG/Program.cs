0e0e0c0ff017e4008b5cb199ed2494b8424a5f9e
song-ssi <song33@outlook.kr>
2025-04-22 16:35:02 +0900

    초기화

--- team17_textRPG/team17_textRPG/Program.cs
+++ team17_textRPG/team17_textRPG/Program.cs
@@ -1,10 +1,1 @@
-﻿namespace team17_textRPG
-{
-    internal class Program
-    {
-        static void Main(string[] args)
-        {
-            Console.WriteLine("Hello, World!");
-        }
-    }
-}
+﻿
